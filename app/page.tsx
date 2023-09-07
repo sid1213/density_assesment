15301df5f94c89430b1c698f4642105f15d4bf8d
sidharth1213 <siddharth.soni@cobold.in>
2023-09-07 16:14:33 +0530

    SS: Reset the setup with sass and removed src file structure

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen ">
+      <h1 className="text-4xl font-semibold">Namsate dev</h1>
+    </main>
+  );
+}
