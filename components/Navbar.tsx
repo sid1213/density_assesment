7f9a023e37122c2f8f8e5586099fd74cb0aeddcf
sidharth1213 <siddharth.soni@cobold.in>
2023-09-07 18:24:09 +0530

    SS: Created Ui for Navbar and set the scss file

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,24 @@
+import Image from "next/image";
 import React from "react";
 
 const Navbar = () => {
-  return <nav className="xl:container mx-auto p-5 ">Navbar</nav>;
+  return (
+    <nav className="navbar  ">
+      <div className="navbar-child">
+        <div>
+          <Image src="/logo.png" width={75} height={75} alt="logo" />
+        </div>
+        <ul className="flex space-x-10">
+          <li>Emotions</li>
+          <li>Manifesto</li>
+          <li>Self-awareness test</li>
+          <li>Work with us</li>
+        </ul>
+        <div>
+          <button className="download-btn"> Download App</button>
+        </div>
+      </div>
+    </nav>
+  );
 };
 export default Navbar;
