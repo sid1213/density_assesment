28744518adc8838d1522c4f0db540d12e4be9669
sidharth1213 <siddharth.soni@cobold.in>
2023-09-08 09:09:51 +0530

    SS: Fixed styling for navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,10 +9,10 @@ const Navbar = () => {
           <Image src="/logo.png" width={75} height={75} alt="logo" />
         </div>
         <ul className="flex space-x-10">
-          <li>Emotions</li>
-          <li>Manifesto</li>
-          <li>Self-awareness test</li>
-          <li>Work with us</li>
+          <li className="text-black">Emotions</li>
+          <li className="text-black">Manifesto</li>
+          <li className="text-black">Self-awareness test</li>
+          <li className="text-black">Work with us</li>
         </ul>
         <div>
           <button className="download-btn"> Download App</button>
