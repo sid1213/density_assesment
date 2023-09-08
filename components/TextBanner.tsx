d266c5cf1849ca2e12ec67689c91504faa2031f0
sidharth1213 <siddharth.soni@cobold.in>
2023-09-08 09:09:39 +0530

    SS: Fixed new section for text banner

--- components/TextBanner.tsx
+++ components/TextBanner.tsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 const TextBanner = () => {
-  return <div></div>;
+  return (
+    <section className="section">
+      <div className="flex p-5 flex-nowrap">
+        <div className="w-1/3 p-5 flex  ">
+          <h3 className="text-2xl font-semibold w-fit mx-auto">EQ beats IQ</h3>
+        </div>
+        <div className="w-1/2 flex p-5 text-lg space-x-4">
+          <p>
+            People with high emotional intelligence (EQ) live more fulfilled
+            lives. They tend to be happier and have healthier relationships.
+          </p>
+          <p>
+            They are more successful in their pursuits and make for inspiring
+            leaders. According to science, they earn $29k a year.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default TextBanner;
