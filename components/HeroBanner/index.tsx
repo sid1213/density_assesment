433427af276cf329ee2d12bd2e44c9b54f9c91ea
sidharth1213 <siddharth.soni@cobold.in>
2023-09-08 21:50:37 +0530

    SS: Added animation on load

--- components/HeroBanner/index.tsx
+++ components/HeroBanner/index.tsx
@@ -1,11 +1,52 @@
+"use client";
+import React, { useEffect } from "react";
 import Image from "next/image";
 import "components/HeroBanner/index.scss";
 import { uuid } from "uuidv4";
+
+import { gsap, CSSPlugin, Expo } from "gsap";
+gsap.registerPlugin(CSSPlugin);
 const HeroBanner = () => {
+  const start = () => {
+    const cloudOne = gsap.timeline(() => console.log("cloud-one"));
+    const cloudTwo = gsap.timeline(() => console.log("cloud-two"));
+    const cloudThree = gsap.timeline(() => console.log("cloud-three"));
+    const bushSingle = gsap.timeline(() => console.log("single-bush "));
+    const bushDouble = gsap.timeline(() => console.log("double-bush "));
+    const bricks = gsap.timeline(() => console.log("bricks"));
+    const text = gsap.timeline({
+      onComplete: () => {
+        console.log("complete");
+      },
+    });
+    text.to("#top", {
+      left: "2%",
+      duration: 1.5,
+    });
+    cloudOne.to(".cloud-one", { right: "20%", duration: 1 });
+    cloudTwo.to(".cloud-two", { right: "40%", duration: 1.5 });
+    cloudThree.to(".cloud-three", { left: "10%", duration: 1 });
+    bushSingle.to(".single-bush", {
+      transform: "translate(50%, -50%) scale(1)",
+      duration: 1.5,
+    });
+    bushDouble.to(".double-bush", {
+      transform: "translate(50%, -50%) scale(1)",
+      duration: 1.5,
+    });
+    bricks.to(".bricks", {
+      transform: "translate(50%, -50%) scale(1)",
+      duration: 1.5,
+    });
+  };
+
+  useEffect(() => {
+    start();
+  });
   return (
     <section className="bg-blue-400 rounded-3xl hero-banner px-10 py-16 relative ">
-      <div className="w-1/2  ">
-        <p className="text-white mario-text">Ahead App</p>
+      <div className="w-1/2 " id="top">
+        <p className="text-white mario-text ">Ahead App</p>
         <h1 className="text-6xl mt-5 font-bold text-white mario-text">
           Master your life <br />
           by mastering <br />
@@ -47,7 +88,7 @@ const HeroBanner = () => {
       <div className="cloud-two objectPlaced"></div>
       <div className="cloud-three objectPlaced"></div>
       <div className="relative flex justify-center bricks objectPlaced">
-        <div className="brick bg-no-repeat bg-contain"></div>
+        <div className="brick bg-no-repeat bg-contain "></div>
         <div className="brick bg-no-repeat bg-contain"></div>
         <div className="question-brick bg-no-repeat bg-contain"></div>
         <div className="brick bg-no-repeat bg-contain"></div>
