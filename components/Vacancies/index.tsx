56f593791c648e14d728543678e1c5bdb8afd2a5
sidharth1213 <siddharth.soni@cobold.in>
2023-09-10 16:03:11 +0530

    SS: Fixed structure

--- components/Vacancies/index.tsx
+++ components/Vacancies/index.tsx
@@ -32,14 +32,16 @@ const vData = [
 
 function Vacancies() {
   return (
-    <section className="vacancies min-h-[50vh] overflow-hidden rounded-3xl lg:px-10 px-5 py-10 relative mt-0">
-      <h2 className="my-10" data-aos="fade-right">
-        Our Mario Devs
-      </h2>
-      <div className="container flex justify-between lg:space-x-10 lg:space-y-0 space-y-10 lg:flex-row flex-col">
-        {vData.map((item) => (
-          <Vcard title={item.title} content={item.content} key={uuid()} />
-        ))}
+    <section className=" section ">
+      <div className="vacancies min-h-[50vh] overflow-hidden rounded-3xl lg:px-10 px-5 py-10 relative mt-0">
+        <h2 className="my-10" data-aos="fade-right">
+          Our Mario Devs
+        </h2>
+        <div className="container flex justify-between lg:space-x-10 lg:space-y-0 space-y-10 lg:flex-row flex-col">
+          {vData.map((item) => (
+            <Vcard title={item.title} content={item.content} key={uuid()} />
+          ))}
+        </div>
       </div>
     </section>
   );
