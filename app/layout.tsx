1ae7d9ae4997542235961be4d9fbdb14853afd2c
sidharth1213 <siddharth.soni@cobold.in>
2023-09-10 15:06:14 +0530

    SS: Prevent scroll always to be at top

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `history.scrollRestoration = "manual"`,
+          }}
+          // added this line of code to prevent scroll should always on top
+        />
         <link rel="icon" href="/favicon.ico" sizes="16x16" />
       </head>
       <body className={inter.className}>
