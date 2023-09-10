1ce0504e8fc207fa92b989f55caf91c69bbde17f
sidharth1213 <siddharth.soni@cobold.in>
2023-09-10 14:21:28 +0530

    SS: Added typing animation with new package

--- components/GetStarted/index.tsx
+++ components/GetStarted/index.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import "./index.scss";
+import { TypeAnimation } from "react-type-animation";
 
 const GetStarted = () => {
   return (
@@ -7,29 +9,39 @@ const GetStarted = () => {
         className="md:w-3/6 mx-auto flex justify-center items-center flex-col text-center"
         data-aos="fade-up"
       >
-        <p className="privacy text-sm font-semibold">
-          We take privacy seriously
-        </p>
+        <p className="privacy  font-semibold">We take privacy seriously</p>
         <h3 className="text-2xl font-bold my-3">
           Before you get started Mario
         </h3>
 
-        <p className="text-lg">
+        <p className=" ">
           &quot;Welcome to the world of Mario! Remember, it&apos;s not just
           about designing levels; it&apos;s about crafting adventures,
           surprises, and fun. Dive in with creativity and enthusiasm! 🍄🌟&quot;
         </p>
 
-        <p className="text-sm font-semibold my-3">
-          with love,
-          <span className="text-base italic text-black">Team Mario</span>
+        <p className=" font-semibold my-3">
+          with love,{" "}
+          <TypeAnimation
+            sequence={[
+              // Same substring at the start will only be typed out once, initially
+              "Team Mario 🍄",
+              1000, // wait 1s before replacing "Mice" with "Hamsters"
+              "With Love ❤️",
+              2000,
+            ]}
+            wrapper="span"
+            speed={50}
+            className=" text-2xl italic spl-text text-black"
+            repeat={Infinity}
+          />
         </p>
 
-        <button className="text-xs px-6 py-3 bg-black text-white rounded-full my-5">
+        <button className=" px-6 py-3 bg-black text-white rounded-full my-5 ">
           Start a Game
         </button>
 
-        <p className="text-[11px] font-semibold"> Enjoy the game</p>
+        <p className=" font-semibold"> Enjoy the game</p>
       </div>
     </section>
   );
