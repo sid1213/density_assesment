21a444d95dbcce69b7e5ecca3d4ddf7d9c9ec5d0
sidharth1213 <siddharth.soni@cobold.in>
2023-09-10 15:12:04 +0530

    SS: Removed unneccesary container

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
       </head>
       <body className={inter.className}>
         <Navbar />
-        <main>{children}</main>
+        {children}
         <Footer />
       </body>
     </html>
