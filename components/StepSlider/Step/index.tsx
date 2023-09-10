b79ee21926a6cb21bf0f895f6dcc5e4c321d505d
sidharth1213 <siddharth.soni@cobold.in>
2023-09-11 00:45:19 +0530

    SS: Updated style and added image for steps

--- components/StepSlider/Step/index.tsx
+++ components/StepSlider/Step/index.tsx
@@ -1,17 +1,28 @@
 import React from "react";
 import "./index.scss";
+import Image from "next/image";
 interface PropType {
   heading: string;
   paragraph: string;
+  color: string;
 }
-const Step: React.FC<PropType> = ({ heading, paragraph }) => {
+const Step: React.FC<PropType> = ({ heading, paragraph, color }) => {
   return (
     <div
-      data-aos="fade-up"
-      className={`space-y-3 pl-10 pb-20 md:w-[70%] w-[90%] mx-auto border-l-4 border-blue-300 dot transition-opacity duration-300`}
+      data-aos="fade-up-left"
+      className={`space-y-3 flex p-5 rounded-2xl  items-center space-x-10  md:w-[70%] w-[90%] mx-auto transition-opacity duration-300`}
+      style={{ background: color }}
     >
-      <div className="">
-        <h4 className="font-semibold translate-y-[-50%] ">{heading}</h4>
+      <Image
+        src={`/mario-flag.png`}
+        width={55}
+        height={55}
+        objectFit="contain"
+        alt="flag"
+      />
+
+      <div className=" space-y-2 ">
+        <h4 className="font-semibold ">{heading}</h4>
         <p className="">{paragraph}</p>
       </div>
     </div>
--- components/StepSlider/index.tsx
+++ components/StepSlider/index.tsx
@@ -9,41 +9,49 @@ const marioData = [
     heading: "Super Mario Bros.",
     paragraph:
       "The classic game that started it all. Join Mario on a quest to rescue Princess Peach from the clutches of Bowser and save the Mushroom Kingdom.",
+    color: "#FFB6C1",
   },
   {
     heading: "Super Mario Bros. 2",
     paragraph:
       "In this sequel, Mario and his friends venture to the dream world of Subcon to defeat the evil Wart. Play as Mario, Luigi, Toad, or Princess Peach.",
+    color: "#87CEEB",
   },
   {
     heading: "Super Mario Bros. 3",
     paragraph:
       "Return to the Mushroom Kingdom with Mario and Luigi in this platforming masterpiece. Battle the Koopalings and save the royal family.",
+    color: "#98FB98",
   },
   {
     heading: "Super Mario Land",
     paragraph:
       "Take Mario on an adventure to the far-off Sarasaland to rescue Princess Daisy from the alien villain Tatanga.",
+    color: "#FFD700",
   },
   {
     heading: "Super Mario 3D World",
     paragraph:
       "Join Mario and friends in a 3D platforming adventure. Use power-ups like the Super Bell and Double Cherry to conquer unique levels.",
+    color: "#FFA07A",
   },
   {
     heading: "Super Mario RPG: Legend of the Seven Stars",
     paragraph:
       "Embark on a role-playing journey with Mario and his allies to defeat the evil Smithy Gang and recover the seven legendary stars.",
+    color: "#F0E68C",
   },
   {
     heading: "Super Mario Galaxy 2",
     paragraph:
       "Continue your cosmic adventure with Mario and Yoshi as they traverse galaxies, solving puzzles and battling cosmic foes.",
+    color: "#DDA0DD",
   },
   {
     heading: "Super Mario Maker",
     paragraph:
       "Become a level designer and create your own Super Mario levels. Share your creations with players around the world.",
+    color: "#B0E0E6",
   },
 ];
 
@@ -58,12 +66,13 @@ const StepSlider = () => {
         </h4>
         <h2 data-aos="fade-right">Step&apos;s Of Mario.</h2>
       </div>
-      <div className="steps mt-20 pt-8" id="steps">
+      <div className="steps mt-10 space-y-10 " id="steps">
         {marioData.map((data) => (
           <Step
             key={uuid()}
             heading={data.heading}
             paragraph={data.paragraph}
+            color={data.color}
           />
         ))}
       </div>
